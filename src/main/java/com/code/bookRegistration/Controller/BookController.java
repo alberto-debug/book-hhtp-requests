96c271923764fbc6aaf5de460dc8d66905e9727e
Alberto <albertojunior848423803@gmail.com>
2025-01-27 23:21:53 +0300

    v

--- src/main/java/com/code/bookRegistration/Controller/BookController.java
+++ src/main/java/com/code/bookRegistration/Controller/BookController.java
@@ -18,31 +18,29 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping("/register")
-    public ResponseEntity<Book> registerBook(@RequestBody Book book){
+    public ResponseEntity<Book> registerBook(@RequestBody Book book) {
         Book registeredBook = bookService.registerBook(book);
         return ResponseEntity.ok(registeredBook);
-
     }
 
     @GetMapping
-    public ResponseEntity<List<Book>> getAllBooks(){
+    public ResponseEntity<List<Book>> getAllBooks() {
         List<Book> books = bookService.getAllBooks();
-        if (books.isEmpty()){
+        if (books.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(books);
     }
 
-
     @GetMapping("/{id}")
-    public ResponseEntity<Book> getBookById(@PathVariable Long id){
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
         return bookService.getBookById(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBookById(@PathVariable Long id){
+    public ResponseEntity<Void> deleteBookById(@PathVariable Long id) {
         boolean deleted = bookService.deleteBookById(id);
         return deleted ? new ResponseEntity<>(HttpStatus.NO_CONTENT) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
