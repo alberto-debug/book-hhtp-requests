e29a3cf8b54da08ed39103916aa75721c8327787
Alberto <albertojunior848423803@gmail.com>
2025-01-27 23:23:14 +0300

    cc

--- src/main/java/com/code/bookRegistration/Controller/BookController.java
+++ src/main/java/com/code/bookRegistration/Controller/BookController.java
@@ -21,7 +21,6 @@ public class BookController {
     public ResponseEntity<Book> registerBook(@RequestBody Book book) {
         Book registeredBook = bookService.registerBook(book);
         return ResponseEntity.ok(registeredBook);
-
     }
 
     @GetMapping
